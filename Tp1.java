9c6b382fad6a56b652890a3ed5fda75faced9f45
Rui Ze Ma <mmrz33@gmail.com>
2019-06-11 15:39:41 -0400

    Initial commit

---
+++ Tp1.java
@@ -0,0 +1,11 @@
+public class Tp1 {
+
+    public static void main(String[] args) {
+	    String input = args[0];
+	    String output = args[1];
+
+	    // TODO: Parsing input file
+        // TODO: Pathfinding algo: nearest location?
+        // TODO: Output file
+    }
+}
